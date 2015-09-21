4b0189612979fd1c34275936f2c8b1617c433d5a
Jiyiwangluo <18815278740@163.com>
2015-09-21 10:53:47 +0800

    3214

--- calendar/controller/CalendarViewController.m
+++ calendar/controller/CalendarViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    UILabel *label = [[UILabel alloc]init];
+    [self.view addSubview:label];
 }
 
 - (void)didReceiveMemoryWarning {
